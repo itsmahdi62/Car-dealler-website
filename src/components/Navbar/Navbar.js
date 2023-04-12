bc50cfe5aef6579dc4f1e1aa7e95e025293b2095
Mahdi <amiralmasi021@gmail.com>
2023-04-12 22:25:20 +0330

    Make navbar responsive

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,12 @@
-import { Link } from "react-router-dom";
-
+import {IoIosCloseCircle} from 'react-icons/io'
+import {TbGridDots} from 'react-icons/tb'
+import "./Navbar.scss"
 //================== import images ================================
 import logo from "../../asset/download.png"
 const Navbar = () => {
     return ( <div className="header">
-     <div className="logoDiv">
-        <img src={logo} alt="" className="logo" style={{height:'100px',width:"100px"}} />
+     <div className="header-logoDiv">
+        <img src={logo} alt="" className="logo" />
      </div>
 
     {/* ==========================the menuBar and it's options====================== */}
@@ -24,9 +25,14 @@ const Navbar = () => {
                 <a href="/#" className="link">Sell</a>
             </li>
         </ul>
+        {/*=============================== add icons to navbar for exit ============================== */}
+        <IoIosCloseCircle className='navbar-icon' />
      </div>
-
+     <div className='signUp flex'>
+        <div className='text'>Sign Up</div>
+        {/*=============================== add icons to navbar for show sign-up ============================== */}
+        <TbGridDots />
+     </div>
     </div> );
 }
- 
 export default Navbar;
