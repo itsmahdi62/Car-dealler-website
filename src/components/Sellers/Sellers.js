ce91362e1f48c3efee7d302e2286453f0c0cb1e6
Mahdi <amiralmasi021@gmail.com>
2023-04-15 01:24:16 +0330

    Create sellers component

--- src/components/Sellers/Sellers.js
+++ src/components/Sellers/Sellers.js
@@ -1,6 +1,69 @@
+import "./Seller.scss"
+
+import defaultImage from "../../asset/homeImg.png"
+
 const Seller = () => {
-    return ( <div className="Seller">
-        
+    return ( 
+    <div className="seller section">
+        <div className="secContainer container">
+            <div className="secHeading grid">
+                <h3 className="secTitle">
+                </h3>
+                <p>Mahdi Almasi holds the record to be the best salesman
+                </p>
+            </div>
+
+            <div className="sellersContainer">
+                {/* Single seller */}
+                <div className="singleSeller flex">
+                    <div className="imgDiv flex">
+                        <img src={defaultImage} alt="" className="img" />
+                    </div>
+                    <span className="info">
+                        <h4 className="name">
+                            Toyota
+                        </h4>
+                        <p>from $40;</p>
+                    </span>
+                </div>
+                {/* Single seller */}
+                <div className="singleSeller flex">
+                    <div className="imgDiv flex">
+                        <img src={defaultImage} alt="" className="img" />
+                    </div>
+                    <span className="info">
+                        <h4 className="name">
+                            Toyota
+                        </h4>
+                        <p>from $40;</p>
+                    </span>
+                </div>
+                {/* Single seller */}
+                <div className="singleSeller flex">
+                    <div className="imgDiv flex">
+                        <img src={defaultImage} alt="" className="img" />
+                    </div>
+                    <span className="info">
+                        <h4 className="name">
+                            Toyota
+                        </h4>
+                        <p>from $40;</p>
+                    </span>
+                </div>
+                {/* Single seller */}
+                <div className="singleSeller flex">
+                    <div className="imgDiv flex">
+                        <img src={defaultImage} alt="" className="img" />
+                    </div>
+                    <span className="info">
+                        <h4 className="name">
+                            Toyota
+                        </h4>
+                        <p>from $40;</p>
+                    </span>
+                </div>
+            </div>
+        </div>
     </div> );
 }
  
