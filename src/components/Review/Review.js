6a29d0b1c966e85b4edb45303782bf69f1753588
Mahdi <amiralmasi021@gmail.com>
2023-04-18 01:56:48 +0330

    Add styles to Reveiw and comments

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,5 +1,7 @@
 import "./Review.scss"
 import {BsArrowLeftShort} from 'react-icons/bs'
+import defaultCar from "../../asset/homeImg.png"
+import {AiFillStar} from "react-icons/ai"
 const Review = () => {
     return ( 
     <div className="review section">
@@ -18,7 +20,7 @@ const Review = () => {
           {/* single reveiw */}
             <div className="singleReview"> 
                 <div className="imgDiv">
-                    <img src="" alt="Car Image" />
+                    <img src={defaultCar} alt="Car Image" />
                 </div>
                 <h5 className="reviewTitle">
                     20233 Cadillac
@@ -31,7 +33,7 @@ const Review = () => {
                 <div className="reviewerDiv flex">
                     <div className="leftDiv flex">
                         <div className="reviewerImage" >
-                            <img src="" alt="Reviewr Image" />
+                            <img src={defaultCar} alt="Reviewr Image" />
                         </div>
                         <div className="aboutReviewer">
                             <span className="name">
@@ -40,8 +42,104 @@ const Review = () => {
                             <p>Chief Editor </p>
                         </div>
                     </div>
+                    <div className="rightDiv flex">
+                        <AiFillStar className='icon' />
+                        <blockquote>4.84</blockquote>
+                    </div>
                 </div>
+            </div>
+            {/* single reveiw */}
+            <div className="singleReview"> 
+                <div className="imgDiv">
+                    <img src={defaultCar} alt="Car Image" />
+                </div>
+                <h5 className="reviewTitle">
+                    20233 Cadillac
+                </h5>
+                <span className="desc">
+                    Good cars are safe , fast , affordable to purchase , economical to 
+                    operate , reliable , cacacious , comfortable , and attractive .  
+                </span>
 
+                <div className="reviewerDiv flex">
+                    <div className="leftDiv flex">
+                        <div className="reviewerImage" >
+                            <img src={defaultCar} alt="Reviewr Image" />
+                        </div>
+                        <div className="aboutReviewer">
+                            <span className="name">
+                                Mahdi Almasi
+                            </span>
+                            <p>Chief Editor </p>
+                        </div>
+                    </div>
+                    <div className="rightDiv flex">
+                        <AiFillStar className='icon' />
+                        <blockquote>4.84</blockquote>
+                    </div>
+                </div>
+            </div>
+            {/* single reveiw */}
+            <div className="singleReview"> 
+                <div className="imgDiv">
+                    <img src={defaultCar} alt="Car Image" />
+                </div>
+                <h5 className="reviewTitle">
+                    20233 Cadillac
+                </h5>
+                <span className="desc">
+                    Good cars are safe , fast , affordable to purchase , economical to 
+                    operate , reliable , cacacious , comfortable , and attractive .  
+                </span>
+
+                <div className="reviewerDiv flex">
+                    <div className="leftDiv flex">
+                        <div className="reviewerImage" >
+                            <img src={defaultCar} alt="Reviewr Image" />
+                        </div>
+                        <div className="aboutReviewer">
+                            <span className="name">
+                                Mahdi Almasi
+                            </span>
+                            <p>Chief Editor </p>
+                        </div>
+                    </div>
+                    <div className="rightDiv flex">
+                        <AiFillStar className='icon' />
+                        <blockquote>4.84</blockquote>
+                    </div>
+                </div>
+            </div>
+            {/* single reveiw */}
+            <div className="singleReview"> 
+                <div className="imgDiv">
+                    <img src={defaultCar} alt="Car Image" />
+                </div>
+                <h5 className="reviewTitle">
+                    20233 Cadillac
+                </h5>
+                <span className="desc">
+                    Good cars are safe , fast , affordable to purchase , economical to 
+                    operate , reliable , cacacious , comfortable , and attractive .  
+                </span>
+
+                <div className="reviewerDiv flex">
+                    <div className="leftDiv flex">
+                        <div className="reviewerImage" >
+                            <img src={defaultCar} alt="Reviewr Image" />
+                        </div>
+                        <div className="aboutReviewer">
+                            <span className="name">
+                                Mahdi Almasi
+                            </span>
+                            <p>Chief Editor </p>
+                        </div>
+                    </div>
+                    <div className="rightDiv flex">
+                        <AiFillStar className='icon' />
+                        <blockquote>4.84</blockquote>
+                    </div>
+                </div>
             </div>
          </div>
 
